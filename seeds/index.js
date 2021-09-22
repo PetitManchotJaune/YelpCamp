ac144de8814df260507a12c643b8ed3b3f28baab
PetitManchotJaune <csantos.doris@gmail.com>
2021-09-22 15:54:16 +0100

    update the model with image property and change price property to be number; update the seeds index file and the show page with image to be an url that will give us a random image (of a collection from unsplash) each we load the page, also with description (lorem for now) and price to be random.

--- seeds/index.js
+++ seeds/index.js
@@ -20,11 +20,15 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
+        const price = Math.floor(Math.random() * 20) + 10;
         // Make a random number to pick a city
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae expedita ducimus officia nam ipsam libero vero velit laboriosam, adipisci maxime dolore, provident, incidunt esse repudiandae est voluptatem amet maiores ipsa. Pariatur nemo hic sed voluptatem, debitis minus beatae quidem, fugiat accusantium a sequi consequuntur cumque cum soluta necessitatibus iste dignissimos fugit suscipit dolorem incidunt dolores reiciendis magni est! Obcaecati, maxime.',
+            price
         })
         await camp.save();
     }
