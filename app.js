b09a359377e0bf1989379e6ae0a9f7ce7e3ef6e9
PetitManchotJaune <csantos.doris@gmail.com>
2021-10-13 21:32:31 +0100

    Joi installation; server side validations applied with Joi on post route; add some informative comments

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
@@ -32,38 +33,64 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
+// Bring all campgrounds
 app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
 }))
 
+// Present the new campground page
 app.get('/campgrounds/new', async (req, res) => {
     res.render('campgrounds/new');
 })
 
+// Create a new campground
 app.post('/campgrounds', catchAsync(async (req, res) => {
-    if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    // This is a logic to see if 'req.body' contains campground at all:
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+
+    // This is going to validate our data before it even attempt to save it with Mongoose:
+    const campgroundSchema = Joi.object({
+        // We are expecting this to be an object and also to be required:
+        campground: Joi.object({
+            title: Joi.string().required(),
+            image: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            description: Joi.string().required(),
+            location: Joi.string().required()
+        }).required()
+    })
+    const { error } = campgroundSchema.validate(req.body);
+    // We are mapping through 'error.details' because 'details' is an an array of objects:
+    const msg = error.details.map(el => el.message).join(',');
+    if (error) {
+        throw new ExpressError(msg, 400);
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
+// Present a details page of a campground
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
 }))
 
+// Present a modification page of a campground
 app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', { campground });
 }))
 
+// Update a campground
 app.put('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
+// Delete a campground
 app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
@@ -83,3 +110,5 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {
     console.log('Serving on port 3000');
 })
+
+// JOI is a schema description language and data validator for Javascript
