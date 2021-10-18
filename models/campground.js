7265c4d2cfde33428eee39661d4ea3c922aee9bf
PetitManchotJaune <csantos.doris@gmail.com>
2021-10-18 19:23:41 +0100

    create a basic schema for reviews and use an array of objects to store many reviews on each campground

--- models/campground.js
+++ models/campground.js
@@ -6,7 +6,13 @@ const CampgroundSchema = new Schema({
     location: String,
     image: String,
     price: Number,
-    description: String
+    description: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
