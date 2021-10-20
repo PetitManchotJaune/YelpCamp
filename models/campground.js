77e83535b64362873b9184db811a461144d13b81
PetitManchotJaune <csantos.doris@gmail.com>
2021-10-20 23:24:25 +0100

    Create campground delete middleware to delete all reviews associated to a campground that was deleted

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require('./review');
 
 const CampgroundSchema = new Schema({
     title: String,
@@ -14,5 +15,17 @@ const CampgroundSchema = new Schema({
         }
     ]
 });
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    console.log(doc);
+    if (doc) {
+        await Review.remove({
+            // To remove all reviews where their id field is in our doc (that one that was just deleted), in its reviews array:
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
